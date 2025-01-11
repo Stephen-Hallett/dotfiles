a8ffe685858644fbf499594f7e7ecc2f1284c489
Stephen-Hallett <stevohallett@gmail.com>
2025-01-11 15:30:34 +1300

    chore: Move homebrew configuration to homebrew file

--- nix-config/nix-darwin/configuration.nix
+++ nix-config/nix-darwin/configuration.nix
@@ -16,37 +16,6 @@
         fantasque-sans-mono
       ];
 
-      homebrew = {
-        enable = true;
-        brews = [
-          "mas"
-        ];
-        casks = [
-          # ----- General -----
-          "google-chrome"
-          "postman"
-          "docker"
-          "vlc"
-          "rstudio"
-          "zoom"
-          "spotify"
-          "discord"
-          "visual-studio-code"
-          # ----- Personal -----
-          "ultimaker-cura"
-          "jagex"
-          "autodesk-fusion"
-          "arduino-ide"
-        ];
-        masApps = {
-          "Notability" = 360593530;
-          "Xcode" = 497799835;
-        };
-        onActivation.autoUpdate = false;
-        onActivation.upgrade = false;
-        onActivation.cleanup = "zap";
-      };
-
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
--- nix-config/nix-darwin/modules/homebrew.nix
+++ nix-config/nix-darwin/modules/homebrew.nix
@@ -6,4 +6,35 @@
     user = "stephen";
     autoMigrate = true;
   };
+
+  homebrew = {
+    enable = true;
+    brews = [
+      "mas"
+    ];
+    casks = [
+      # ----- General -----
+      "google-chrome"
+      "postman"
+      "docker"
+      "vlc"
+      "rstudio"
+      "zoom"
+      "spotify"
+      "discord"
+      "visual-studio-code"
+      # ----- Personal -----
+      "ultimaker-cura"
+      "jagex"
+      "autodesk-fusion"
+      "arduino-ide"
+    ];
+    masApps = {
+      "Notability" = 360593530;
+      "Xcode" = 497799835;
+    };
+    onActivation.autoUpdate = false;
+    onActivation.upgrade = false;
+    onActivation.cleanup = "zap";
+  };
 }
