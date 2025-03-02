61ea064e9f26dca9e05b28d557190bcc99bf5fa2
Stephen-Hallett <stevohallett@gmail.com>
2025-03-02 19:31:36 +1300

    chore: Add useful utils to common config

--- nix-config/hosts/common/home.nix
+++ nix-config/hosts/common/home.nix
@@ -27,6 +27,16 @@
       gnumake
       cmake
       zlib
+      nfs-utils
+      cifs-utils
+      usbutils
+      pciutils
+      nettools
+      # Docker
+      docker
+      docker-compose
+      containerd
+      docker-buildx
     ];
 
     sessionVariables = {
