06b862c6d92736fcfc6b5eacae05b33b0c24a6e9
Stephen-Hallett <stevohallett@gmail.com>
2025-05-25 19:17:30 +1200

    feat: Add proton to steam config

--- nix-config/NixModules/programs/steam.nix
+++ nix-config/NixModules/programs/steam.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }: {
+{ lib, config, pkgs, ... }: {
   options = { nix-config.steam.enable = lib.mkEnableOption "enable steam"; };
 
   config = lib.mkIf config.nix-config.steam.enable {
@@ -7,6 +7,8 @@
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
       localNetworkGameTransfers.openFirewall = true;
+      gamescopeSession.enable = true;
+      extraCompatPackages = with pkgs; [ proton-ge-bin ];
     };
   };
 }
