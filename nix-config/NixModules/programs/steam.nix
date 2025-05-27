575137926f00fb5f2a36e282b94df703d271c298
Stephen-Hallett <stevohallett@gmail.com>
2025-05-27 20:38:47 +1200

    feat: Get sunshine to work

--- nix-config/NixModules/programs/steam.nix
+++ nix-config/NixModules/programs/steam.nix
@@ -2,13 +2,32 @@
   options = { nix-config.steam.enable = lib.mkEnableOption "enable steam"; };
 
   config = lib.mkIf config.nix-config.steam.enable {
-    programs.steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-      gamescopeSession.enable = true;
-      extraCompatPackages = with pkgs; [ proton-ge-bin ];
+    programs = {
+      steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
+        gamescopeSession.enable = true;
+        extraCompatPackages = with pkgs; [ proton-ge-bin ];
+      };
+
+      gamemode.enable = true;
+
+      gamescope.enable = true;
+      gamescope.capSysNice = true;
+      gamescope.args = [
+        "--adaptive-sync" # VRR support
+        "--hdr-enabled" # HDR
+        "--rt" # Real time scheduling
+        "--force-grab-cursor"
+        "-e"
+        "-f" # fullscreen
+        "-W 1920"
+        "-H 1080"
+        "-r 60" # refresh rate
+        # "-O HDMI-A-1" # Monitor
+      ];
     };
   };
 }
