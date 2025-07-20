6d65db97ec5cef800b3014dbcfe1a2b4bbd2caf6
Stephen-Hallett <stevohallett@gmail.com>
2025-07-20 17:32:54 +1200

    feat: Add moonlight client

--- Hosts/macbook/DarwinModules/homebrew.nix
+++ Hosts/macbook/DarwinModules/homebrew.nix
@@ -18,12 +18,12 @@
       "autodesk-fusion"
       "arduino-ide"
       "nextcloud"
+      "moonlight"
     ];
     masApps = {
       "Notability" = 360593530;
       "Xcode" = 497799835;
       "Infuse" = 1136220934;
-      "Moonlight" = 1000551566;
     };
     onActivation.autoUpdate = true;
     onActivation.upgrade = true;
