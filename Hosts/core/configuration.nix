60fd3d5411ad8dda26afe0d2e2d6f776b539c5db
Stephen-Hallett <stevohallett@gmail.com>
2025-08-06 20:25:19 +1200

    feat: Add machines to sops

--- Hosts/core/configuration.nix
+++ Hosts/core/configuration.nix
@@ -72,10 +72,11 @@
     defaultSopsFile = ../../secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
     age = {
-      sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       generateKey = true;
       keyFile = "/home/stephen/.config/sops/age/keys.txt";
     };
+  };
 
   system.stateVersion = "25.05"; # Did you read the comment?
 }
